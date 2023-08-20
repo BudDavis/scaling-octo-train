605516b48feb8ade565d94421eed82871df9cd35
Bud Davis <bdavis9659@gmail.com>
2023-08-20 18:00:20 +0000

    clean up

--- JSBSimEdit/src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
+++ JSBSimEdit/src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
@@ -1,40 +1,44 @@
 package uta.cse.cse3310.JSBSimEdit;
 
-import java.io.File;  
-import java.util.List; 
+import java.io.File;
+import java.util.List;
 
-import jakarta.xml.bind.JAXBContext;  
-import jakarta.xml.bind.JAXBException;  
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.Marshaller;
 import generated.FdmConfig;
-import generated.Propulsion;
-
-/**
- * Hello world!
- *
- */
+ 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //TabbedPaneDemo.mainx();
-        
-        try {  
-   
-            File file = new File("f16.xml");  
-            JAXBContext jaxbContext = JAXBContext.newInstance(FdmConfig.class);  
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    // TabbedPaneDemo.mainx();
+
+    try {
+
+      File file = new File("f16.xml");
+      JAXBContext jaxbContext = JAXBContext.newInstance(FdmConfig.class);
+
+      Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+      FdmConfig cfg = (FdmConfig) jaxbUnmarshaller.unmarshal(file);
+
+      System.out.println(cfg.getName());
        
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();  
-            FdmConfig cfg= (FdmConfig) jaxbUnmarshaller.unmarshal(file);  
-              
-            System.out.println(cfg.getName());  
-            //System.out.println("Answers:");  
-            //List<Answer> list=que.getAnswers();  
-            //for(Answer ans:list)  
-            //  System.out.println(ans.getId()+" "+ans.getAnswername()+"  "+ans.getPostedby());  
-       
-          } catch (JAXBException e) {  
-            e.printStackTrace();  
-          }  
-   
+
+      JAXBContext jc = JAXBContext.newInstance("generated");
+      Marshaller m = jc.createMarshaller();
+      m.marshal(cfg, System.out);
+
+
+      // unmarshal from foo.xml
+      //Unmarshaller u = jc.createUnmarshaller();
+      // FooObject fooObj = (FooObject) u.unmarshal(new File("f16.xml"));
+
+      // marshal to System.out
+     
+    } catch (JAXBException e) {
+      e.printStackTrace();
     }
+
+  }
 }
