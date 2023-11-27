ea44fc8b36bc1586205907980bc939808aad148a
sss <jimmie.davis@uta.edu>
2023-11-27 13:44:36 -0600

    Added some debug code.

--- JSBSimEdit/src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
+++ JSBSimEdit/src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
@@ -11,34 +11,39 @@ import generated.FdmConfig;
 import generated.Force;
 import generated.Location;
 import generated.Direction;
+import generated.FlightControl;
+import generated.Channel;
+import generated.Summer;
+import generated.Kinematic;
 
 
 public class App {
+
+  public class Display {
+	 public Display draw(Display D){
+		 return null;
+	 } 
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    TabbedPaneDemo.mainx();
+    //TabbedPaneDemo.mainx();
 
     try {
 
       File file = new File("f16.xml");
-      // JAXBContext jaxbContext = JAXBContext.newInstance(FdmConfig.class);
-
-      // Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-      // FdmConfig cfg = (FdmConfig) jaxbUnmarshaller.unmarshal(file);
-
-      // System.out.println(cfg.getName());
 
       JAXBContext jc = JAXBContext.newInstance("generated");
 
       Unmarshaller um = jc.createUnmarshaller();
       FdmConfig cfg = (FdmConfig) um.unmarshal(file);
     
-      System.out.println(cfg);
-      System.out.println(cfg.getFileheader().getCopyright());
-      System.out.println(cfg.getFileheader().getVersion() );
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getName());
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getFunction());
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getClass());
+      //System.out.println(cfg);
+      //System.out.println(cfg.getFileheader().getCopyright());
+      //System.out.println(cfg.getFileheader().getVersion() );
+      //System.out.println(cfg.getAerodynamics().getAxis().get(0).getName());
+      //System.out.println(cfg.getAerodynamics().getAxis().get(0).getFunction());
+      //System.out.println(cfg.getAerodynamics().getAxis().get(0).getClass());
 
 
       Force F = new Force();
@@ -63,9 +68,64 @@ public class App {
       cfg.getExternalReactions().getForce().add(F);
       cfg.getExternalReactions().getProperty().add("cromulent");
 
+
+
+      FlightControl fc = cfg.getFlightControl();
+
+      System.out.println("Channels are");
+      for ( Channel ch : fc.getChannel())
+      {
+	      System.out.println(ch.getName());
+	      for (Object o :ch.getActuatorOrAerosurfaceScaleOrIntegrator())
+	      {
+	         //System.out.print("    ");
+		 if (o instanceof Summer)
+		 {
+			 Summer S = (Summer) o;
+	                 System.out.print(" summer ");
+	                 System.out.print(S.getName());
+	                 System.out.println();
+			 System.out.println("   ");
+			 for (String I :S.getInput())
+			 {
+				 System.out.print("     ");
+				 System.out.println(I);
+			 }
+		 }
+		 if (o instanceof Kinematic)
+		 {
+			 Kinematic S = (Kinematic) o;
+	                 System.out.print(" kinematic ");
+	                 System.out.print(S.getName());
+			 System.out.println(S.getInput());
+		 }
+                 System.out.println();
+
+        //@XmlElement(name = "actuator", type = Actuator.class),
+        //@XmlElement(name = "aerosurface_scale", type = AerosurfaceScale.class),
+        //@XmlElement(name = "integrator", type = Integrator.class),
+        //@XmlElement(name = "kinematic", type = Kinematic.class),
+        //@XmlElement(name = "lag_filter", type = LagFilter.class),
+        //@XmlElement(name = "lead_lag_filter", type = LeadLagFilter.class),
+        //@XmlElement(name = "pid", type = Pid.class),
+        //@XmlElement(name = "pure_gain", type = PureGain.class),
+        //@XmlElement(name = "scheduled_gain", type = ScheduledGain.class),
+        //@XmlElement(name = "second_order_filter", type = SecondOrderFilter.class),
+        //@XmlElement(name = "sensor", type = Sensor.class),
+        //@XmlElement(name = "summer", type = Summer.class),
+        //@XmlElement(name = "switch", type = Switch.class),
+        //@XmlElement(name = "washout_filter", type = WashoutFilter.class),
+        //@XmlElement(name = "deadband", type = Deadband.class),
+        //@XmlElement(name = "fcs_function", type = FcsFunction.class)
+
+                 //System.out.println(o.getInput());
+	      }
+      }
+
+
       Marshaller m = jc.createMarshaller();
       m.setProperty("jaxb.formatted.output", true);
-      m.marshal(cfg, System.out);
+      //m.marshal(cfg, System.out);
 
     } catch (JAXBException e) {
       e.printStackTrace();
